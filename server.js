575fc7a59423b5b3f5880fd5d240bcc0d3956f75
PRamsey02 <prestonkramsey02@icloud.com>
2022-10-28 21:22:20 -0500

    Added get, post, and listen

--- server.js
+++ server.js
@@ -10,3 +10,29 @@ const allNotes = require('./db/db.json');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(allNotes.slice(1))
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = createNewNote(req.body, allNotes);
+  res.json(newNote);
+});
+
+
+app.listen(PORT, () =>
+  console.log(`API server listening on port ${PORT}.`)
+);
