2303e6a00fa83b47f9a17450933ab0ce6c291d17
PRamsey02 <prestonkramsey02@icloud.com>
2022-10-28 21:54:02 -0500

    Added delete, notes array

--- server.js
+++ server.js
@@ -32,7 +32,46 @@ app.post('/api/notes', (req, res) => {
   res.json(newNote);
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+  deleteNote(req.params.id, allNotes);
+  res.json(true);
+});
 
-app.listen(PORT, () =>
+function createNewNote(body, notesArray) {
+  const newNote = body;
+  if (!Array.isArray(notesArray))
+      notesArray = [];
+  
+  if (notesArray.length === 0)
+      notesArray.push(0);
+
+  body.id = notesArray[0];
+  notesArray[0]++;
+
+  notesArray.push(newNote);
+  fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify(notesArray, null, 2)
+  );
+  return newNote;
+}
+
+function deleteNote(id, notesArray) {
+  for (let i = 0; i < notesArray.length; i++) {
+      let note = notesArray[i];
+
+      if (note.id == id) {
+          notesArray.splice(i, 1);
+          fs.writeFileSync(
+              path.join(__dirname, './db/db.json'),
+              JSON.stringify(notesArray, null, 2)
+          );
+
+          break;
+      }
+  }
+};
+
+app.listen(PORT, () => {
   console.log(`API server listening on port ${PORT}.`)
-);
+});
